45bb171f3d0821eec3ab6d80feac93e722fbcada
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-07-27 11:21:38 -0300

    feat: adiciona exemplo de uso do laço for-each com vetor

---
+++ VetoresListasMatrizes/src/vetores/application/LacoForEach.java
@@ -0,0 +1,11 @@
+package vetores.application;
+
+public class LacoForEach {
+    public static void main(String[] args) {
+        String[] vect = new String[]{"Maria", "Bob", "Alex"};
+
+        for (String obj : vect) {
+            System.out.println(obj);
+        }
+    }
+}
