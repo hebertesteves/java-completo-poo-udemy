b149461fc9e483d8af2e6d3b826828790e6ebae6
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-08-01 18:26:38 -0300

    feat: implementa classe abstrata Shape com atributo color e método area abstrato

---
+++ HerancaPolimorfismo/src/exemplo5/entities/Shape.java
@@ -0,0 +1,24 @@
+package exemplo5.entities;
+
+import exemplo5.entities.enums.Color;
+
+public abstract class Shape {
+    private Color color;
+
+    public Shape() {
+    }
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area();
+}
