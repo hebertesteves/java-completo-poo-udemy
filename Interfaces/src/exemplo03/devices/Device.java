8610d130ba70f088b2526e4503cc74e2d432fcd6
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-08-15 15:05:24 -0300

    feat: criar classe abstrata Device com serialNumber e processamento de documento

---
+++ Interfaces/src/exemplo03/devices/Device.java
@@ -0,0 +1,15 @@
+package exemplo03.devices;
+
+public abstract class Device {
+    private String serialNumber;
+
+    public Device(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public abstract void processDoc(String doc);
+}
