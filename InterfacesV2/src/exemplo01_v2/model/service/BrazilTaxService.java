af5a76699f5fb28134918b2ef826feaa615165b7
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-09-07 14:52:21 -0300

    feat: implementar serviço de imposto específico do Brasil com regras progressivas

---
+++ InterfacesV2/src/exemplo01_v2/model/service/BrazilTaxService.java
@@ -0,0 +1,12 @@
+package exemplo01_v2.model.service;
+
+public class BrazilTaxService implements TaxService {
+    @Override
+    public double tax(double amount) {
+        if (amount <= 100.0) {
+            return amount * 0.20;
+        } else {
+            return amount * 0.15;
+        }
+    }
+}
