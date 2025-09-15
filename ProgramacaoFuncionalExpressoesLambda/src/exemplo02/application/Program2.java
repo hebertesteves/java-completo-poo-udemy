5d2fb2296c353af7e2f6159cf8ede38553fa242c
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-09-15 17:12:34 -0300

    feat: cria classe Program2 com exemplo de funções como objetos de primeira ordem utilizando referência a método para ordenar produtos no módulo ProgramacaoFuncionalExpressoesLambda

---
+++ ProgramacaoFuncionalExpressoesLambda/src/exemplo02/application/Program2.java
@@ -0,0 +1,25 @@
+package exemplo02.application;
+
+import exemplo02.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program2 {
+    public static int compareProducts(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+    }
+
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        // Funções são objetos de primeira ordem
+        list.sort(Program2::compareProducts);
+
+        list.forEach(System.out::println);
+    }
+}
