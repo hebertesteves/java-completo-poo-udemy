e87b900151eddd6d89cbec19ab3378347ae6d46a
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-10-10 01:39:56 -0300

    feat: adiciona ProductComparator (Comparator por preço)

---
+++ ProgramacaoFuncionalExpressoesLambda/src/exercicios/application/ProductComparator.java
@@ -0,0 +1,12 @@
+package exercicios.application;
+
+import exercicios.entities.Product;
+
+import java.util.Comparator;
+
+public class ProductComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+    }
+}
