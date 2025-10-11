078c3065ea44ebcbce4a3574a166a83949e398c8
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-10-11 16:36:22 -0300

    refactor: separa cálculo das somas e ajusta impressão formatada dos resultados

--- ProgramacaoFuncionalExpressoesLambda/src/exemplo06/application/Program.java
+++ ProgramacaoFuncionalExpressoesLambda/src/exemplo06/application/Program.java
@@ -19,6 +19,8 @@ public class Program {
 
         ProductService ps = new ProductService();
         double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
-        double sum1 = ps.filteredSum(list, p -> p.getPrice() > 100.0); System.out.println("Sum = " + String.format("%.2f", sum)); System.out.println("Sum = " + String.format("%.2f", sum1));
+        double sum1 = ps.filteredSum(list, p -> p.getPrice() > 100.0);
+        System.out.println("Sum = " + String.format("%.2f", sum));
+        System.out.println("Sum = " + String.format("%.2f", sum1));
     }
 }
