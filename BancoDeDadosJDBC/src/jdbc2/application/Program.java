983bcea1de9ad74d520effecafb8f4f676191fe0
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-10-13 20:58:12 -0300

    feat: cria classe Program para executar consulta SQL e exibir dados de departamentos

---
+++ BancoDeDadosJDBC/src/jdbc2/application/Program.java
@@ -0,0 +1,37 @@
+package jdbc2.application;
+
+import jdbc2.db.DB;
+import jdbc2.db.DbException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+    public static void main(String[] args) {
+        // Buscar Dados
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+            DB.closeConnection();
+        }
+    }
+}
